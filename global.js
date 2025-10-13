9dcff58ff8c52752c46a98fc538cb1a14977cc87
Jaden Vo <jaden.p.vo@gmail.com>
2025-10-13 13:30:00 -0700

    added color theme switcher, and revamped navbar using js

--- global.js
+++ global.js
@@ -5,10 +5,10 @@ function $$(selector, context = document) {
 }
 
 let pages = [
-  { url: "", title: "Home" },
-  { url: "projects/", title: "Projects" },
-  { url: "resume/", title: "Resume" },
-  { url: "contact/", title: "Contact" },
+  { url: "/", title: "Home" },
+  { url: "/projects/", title: "Projects" },
+  { url: "/resume/", title: "Resume" },
+  { url: "/contact/", title: "Contact" },
 ];
 
 let nav = document.createElement("nav");
@@ -28,9 +28,37 @@ let currentLink = navLinks.find(
 );
 currentLink?.classList.add("current");
 
-const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "/"                  // Local server
-  : "/website/";         // GitHub Pages repo name
-  if (!url.startsWith('http')) {
-  url = !url.startsWith('http') ? BASE_PATH + url : url;
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+            <option value="light dark">Automatic</option>
+			<option value="light">Light</option>
+			<option value="dark">Dark</option>
+		</select>
+	</label>`,
+);
+
+const select = document.querySelector('.color-scheme select');
+
+// apply color scheme and save to localStorage
+function setColorScheme(theme) {
+  document.documentElement.style.setProperty('color-scheme', theme);
+  select.value = theme;
+  localStorage.colorScheme = theme;
 }
+
+// page load: check localStorage or system preference
+if ('colorScheme' in localStorage) {
+  setColorScheme(localStorage.colorScheme);
+} else {
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  setColorScheme(prefersDark ? 'dark' : 'light');
+}
+
+// handle user changes
+select.addEventListener('input', (event) => {
+  setColorScheme(event.target.value);
+});
