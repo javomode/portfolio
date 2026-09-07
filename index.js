3e260e130dd9b73be82a5160dd9a06687da18ab4
Jaden Vo <jaden.p.vo@gmail.com>
2026-09-06 19:00:33 -0700

    Changed styling on homepage, includes navigation buttons

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects, initGlobal, fetchGitHubData } from './global.js';
+import { fetchJSON, renderProjects, initGlobal } from './global.js';
 
 // initialize nav, color scheme, forms
 initGlobal();
@@ -9,18 +9,3 @@ const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
-
-const githubData = await fetchGitHubData('javomode');
-const profileStats = document.querySelector('#profile-stats');
-
-if (profileStats) {
-  profileStats.innerHTML = `
-        <h3>GitHub: ${githubData.login}</h3>
-        <dl>
-          <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-          <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-          <dt>Followers:</dt><dd>${githubData.followers}</dd>
-          <dt>Following:</dt><dd>${githubData.following}</dd>
-        </dl>
-  `;
-}
